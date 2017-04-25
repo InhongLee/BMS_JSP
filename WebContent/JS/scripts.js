644b96abfe594d3f2031a3e63280547e5bd422ee
InhongLee <amaco78@gmail.com>
2017-04-26 01:37:11 +0900

    기본 구조 연결 완료

--- WebContent/JS/scripts.js
+++ WebContent/JS/scripts.js
@@ -1,6 +1,54 @@
 /**
  * 
  */
-window.onload = function(){
- 
-}
+$(document).ready(function() {
+		$(window).load(function() {
+			$(".sub_menu").hide();
+			$('#section01').load('viewHome.html');
+			return false;
+		});
+		
+		$(".navi a").click(function() {
+			return false;
+		});
+		
+		$("#sales").mouseover(function() {
+			$("#sub_sales").show();
+		});
+		$("#sales").mouseout(function() {
+			$("#sub_sales").hide();
+		});
+		
+		$("#stock").mouseover(function() {
+			$("#sub_stock").show();
+		});
+		$("#stock").mouseout(function() {
+			$("#sub_stock").hide();
+		});
+		
+		$("#order").mouseover(function() {
+			$("#sub_order").show();
+		});
+		$("#order").mouseout(function() {
+			$("#sub_order").hide();
+		});
+		
+		$("#member").mouseover(function() {
+			$("#sub_member").show();
+		});
+		$("#member").mouseout(function() {
+			$("#sub_member").hide();
+		});
+		
+		$("#account").mouseover(function() {
+			$("#sub_account").show();
+		});
+		$("#account").mouseout(function() {
+			$("#sub_account").hide();
+		});
+		$('.navi a').click(function() {
+			var url = $(this).attr('href');
+			$('#section01').load(url);
+			return false;
+		});
+	});
